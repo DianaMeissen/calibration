cf2bd1c4ae065934e0cdc0dfd029c502f8be7cf4
Darya Vyshniakova <vishnya054@gmail.com>
2019-09-18 16:49:56 +0300

    test commit

--- src/mainApp/App.js
+++ src/mainApp/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './mainApp/App.css';
+import '../mainApp/App.css';
 
 import AnimationOfCalibration from '../animationOfCalibration/AnimationOfCalibration';
 import HelloBlock from '../helloBlock/HelloBlock';
