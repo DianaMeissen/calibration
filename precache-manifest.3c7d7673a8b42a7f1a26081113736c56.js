d97f2107424ce1909573143c6a526dbc08951f66
Darya Vyshniakova <vishnya054@gmail.com>
2019-09-18 16:47:54 +0300

    Updates

---
+++ precache-manifest.3c7d7673a8b42a7f1a26081113736c56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601849d0e09fedb084914377b074d3e1",
+    "url": "/DianaMeissen/calibration.git/index.html"
+  },
+  {
+    "revision": "9fb2ee9134e9c4971c8c",
+    "url": "/DianaMeissen/calibration.git/static/css/main.d48e82e1.chunk.css"
+  },
+  {
+    "revision": "287d9161eadb5a37f339",
+    "url": "/DianaMeissen/calibration.git/static/js/2.27f79297.chunk.js"
+  },
+  {
+    "revision": "9fb2ee9134e9c4971c8c",
+    "url": "/DianaMeissen/calibration.git/static/js/main.f7c31f6d.chunk.js"
+  },
+  {
+    "revision": "c28b9dcba074f6844f74",
+    "url": "/DianaMeissen/calibration.git/static/js/runtime~main.53ac1544.js"
+  }
+]);
